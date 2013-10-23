4f9f6f731277ee5c9d64867377eea08200a7be35
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-23 18:25:15 +0100

    comentando para hacer las pruebas

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -16,67 +16,65 @@ class Fraction
       @den = den / common
     end
   end
-  def num() 
-    @num
-  end
-
-  def den()
-    @den
-  end
-  
-  def to_string()
-    "#{@num}/#{@den}"
-  end
-
-  def to_float()
-    flotante =@num.to_f/@den.to_f
-    flotante
-  end
- 
-  
-  def +(other)
-    Fraction.new(@num* other.den + other.num*@den, @den * other.den)
-  end
-  
-  def -(other)
-  end
-  
-  def *(other)
-    Fraction.new(@num * other.num, @den *other.den)
-  end
-  
-  def \(other)
-    Fraction.new(@num * other.den, @den *other.num)
-  end
-  
-  def %
-    @num % @den
-  end
-  
-  def abs()
-    Fraction.new(@num.abs, @den.abs)
-  end
-  
-  def -@
-    Fraction.new(@num * -1, @den)
-  end
-  
-  def reciprocal()
-    Fraction.new(@den, @num)
-  end
-  
-  def ==(other)
-    if(@num == other.num) && (@den == other.den)
-      true
-    else
-      false
-    end
-  end
-  
-  def <=>(other)    
-    to_float <=> other.to_float    
-  end
-     
+#   def num() 
+#     @num
+#   end
+# 
+#   def den()
+#     @den
+#   end
+#   
+#   def to_string()
+#     "#{@num}/#{@den}"
+#   end
+# 
+#   def to_float()
+#     flotante =@num.to_f/@den.to_f
+#     flotante
+#   end
+#  
+#   
+#   def +(other)
+#     Fraction.new(@num* other.den + other.num*@den, @den * other.den)
+#   end
+#   
+#   def -(other)
+#   end
+#   
+#   def *(other)
+#     Fraction.new(@num * other.num, @den *other.den)
+#   end
+#   
+#   def \(other)
+#     Fraction.new(@num * other.den, @den *other.num)
+#   end
+#   
+#   def %
+#     @num % @den
+#   end
+#   
+#   def abs()
+#     Fraction.new(@num.abs, @den.abs)
+#   end
+#   
+#   def -@
+#     Fraction.new(@num * -1, @den)
+#   end
+#   
+#   def reciprocal()
+#     Fraction.new(@den, @num)
+#   end
+#   
+#   def ==(other)
+#     if(@num == other.num) && (@den == other.den)
+#       true
+#     else
+#       false
+#     end
+#   end
+#   
+#   def <=>(other)    
+#     to_float <=> other.to_float    
+#   end
+#      
 end
-  
-  
