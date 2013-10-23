966e909a8a8d2ae9eb44542224be34528e9afa28
Carlota Lázaro <alu0100698862@ull.edu.es>
2013-10-23 18:32:41 +0100

    Almacenamiento de variables

--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -2,7 +2,22 @@ re 'lib/fraction'
 
 describe Fraction do
   before :each do
-    @f1 = Fraction.new(3,4) 
+    @f1 = Fraction.new(1,4) 
     @f2 = Fraction.new(4,6)
   end
 end
+
+describe " *Almacenamiento del numerador y denominador: " do
+   it "Almacenamiento del numerador" do
+	@f1.num.should eq(1)
+   end
+
+   it "Almacenamiento del denominador" do
+	@f2.den.should eq(4)
+   end
+
+end
+
+
+
+
