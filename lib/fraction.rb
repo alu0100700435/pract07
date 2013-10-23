4666758289c3422d8e223ce23e1876939b0f13f9
Carlota Lázaro <alu0100698862@ull.edu.es>
2013-10-23 16:02:21 +0100

    Inicializacion de variables

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -3,6 +3,18 @@
 class Fraction
   attr_reader :num, :den
   
+  def initalize(num, den)
+    @num, @den = num, den
+  end
+  
+  def =(num)
+    @num = num
+  end
+  
+  def =(den)
+    @den = den
+  end
+  
 end
   
   
