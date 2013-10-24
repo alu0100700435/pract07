611f6443bccd4d3064a97d0d4e0a4067b29acac5
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-24 09:49:42 +0100

    Añadiendo el describe de num() y den()

--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -9,18 +9,27 @@ describe Fraction do
 
 
   describe "#Almacenamiento del numerador y denominador: " do
-    it "Almacenamiento del numerador" do
+    it "Almacenamiento ok del numerador" do
 	@f1.num.should eq(1)
     end
-    it "Almacenamiento del denominador" do
+    it "Almacenamiento ok del denominador" do
 	@f1.den.should eq(4)
     end
-    it "Almacenamiento minimo del numerador" do
+    it "Almacenamiento ok minimo del numerador" do
       @f2.num.should eq(2)
     end
-    it "Almacenamiento minimo del denominador" do
+    it "Almacenamiento ok minimo del denominador" do
       @f2.den.should eq(3)
     end 
+  end
+  
+  describe "#Llamadas a los metodos num() y den():" do
+    it "Obtencion del numerador" do
+      @f2.num().should eq(2)
+    end
+    it "Obtencion del denominador" do
+      @f2.den().should eq(3)
+    end
   end
   
 end
