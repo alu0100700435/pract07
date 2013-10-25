755c97bef6b7d1289105d3e24ba711bca9134d0d
alu0100700435 <alu0100700435@ull.edu.es>
2013-10-25 15:00:26 +0000

    añadiendo el other que se fue

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -68,7 +68,7 @@ class Fraction
      Fraction.new(@den, @num)
    end
    
-   def reciprocal_div()
+   def reciprocal_div(other)
      Fraction.new( @den *other.num, @num * other.den)
    end
    
